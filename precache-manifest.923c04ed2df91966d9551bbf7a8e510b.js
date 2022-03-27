62f2d4b6f6b578e00a7f334d1c7bf2d0d1d27315
NikhilKDhande <100127292+NikhilKDhande@users.noreply.github.com>
2022-03-27 15:42:37 +0530

    Updates

---
+++ precache-manifest.923c04ed2df91966d9551bbf7a8e510b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb98e3e355857a70534cd5ba2f728a77",
+    "url": "/simple-react-previewer/index.html"
+  },
+  {
+    "revision": "ec9aa1e30dd0167af1d1",
+    "url": "/simple-react-previewer/static/css/main.e81533e5.chunk.css"
+  },
+  {
+    "revision": "4973bcc28c8d6cb2b638",
+    "url": "/simple-react-previewer/static/js/2.05e71c12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-previewer/static/js/2.05e71c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9aa1e30dd0167af1d1",
+    "url": "/simple-react-previewer/static/js/main.8fdb639d.chunk.js"
+  },
+  {
+    "revision": "06f1433554b2383ebe95",
+    "url": "/simple-react-previewer/static/js/runtime-main.f4a4591c.js"
+  }
+]);
